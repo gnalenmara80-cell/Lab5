1ebd7b9eaf15223fa6d52f3c476405580b3d00a6
Gnalen Mara <gnalenmara80@gmail.com>
2026-02-13 20:05:06 -0500

    Add function to check dice values and return slang term

--- Lab5_gnalen-1.py
+++ Lab5_gnalen-1.py
@@ -6,3 +6,30 @@ conditional statements to print the appropriate term for that roll based on the 
 Date: 02/13/2026
 
 """
+import random
+def get_term(d1, d2, total):
+    if d1 == 1 and d2 == 1:
+        return "Snake Eyes"
+    elif (d1 == 1 and d2 == 2) or (d1 == 2 and d2 == 1):
+        return "Ace Cought a Deuce"
+    elif d1 == 2 and d2 == 2:
+        return "little Joe from Kokomo"
+    elif (d1 == 1 and d2 == 4) or (d1 == 4 and d2 == 1):
+        return "Little Phoebe"
+    elif d1 == 3 and d2 == 3:
+        return "Jimmy Hicks from the sticks"
+    elif (d1 == 6 and d2 == 1) or (d1 == 1 and d2 == 6):
+        return "Six Ace"
+    elif d1 == 4 and d2 == 4:
+        return "Eighter from Decatur"
+    elif (d1 == 3 and d2 == 6) or (d1 == 6 and d2 == 3):
+        return " Nina from Pasadena"
+    elif d1 == 5 and d2 == 5:
+        return "Puppy Paws"
+    elif (d1 == 6 and d2 == 5) or (d1 == 5 and d2 == 6):
+        return "Six Five No Jive"
+    elif d1 == 6 and d2 == 6:
+        return "BoxCars"
+    else:
+        return "No special term"
+    
