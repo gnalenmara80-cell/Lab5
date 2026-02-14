d966f86bc82ca5e864325a7513ab272839e55bd4
Gnalen Mara <gnalenmara80@gmail.com>
2026-02-13 19:33:19 -0500

    Add initial file with program header and import code

---
+++ Lab5_gnalen-1.py
@@ -0,0 +1,8 @@
+"""
+Lab 5 - Dice Rolling Terms
+Author: Gnalen Mara
+Purpose: This code write a program that rolls two dice, prints the outcome, and then uses
+conditional statements to print the appropriate term for that roll based on the table below.
+Date: 02/13/2026
+
+"""
